0a347ee3fc5675c5dc0de08e4277442db2697e91
Pedro Puppin <puppin.pedro@gmail.com>
2024-04-05 00:07:11 -0300

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins, Lusitana } from 'next/font/google';
+ 
+export const poppins = Poppins({ 
+  weight: ['400', '700'],
+  subsets: ['latin'] 
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
